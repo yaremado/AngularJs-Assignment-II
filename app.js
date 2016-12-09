6acb8594fc96fb20e77df2a3fa4e097ced185e1e
Dima <yaremado@gmail.com>
2016-12-09 20:51:44 +0200

    Assignment II review ready

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
 (function(){
 'use strict'
 
-angular module('ShoppingListCheckOff',[])
+angular.module('ShoppingListCheckOff',[])
 
 .controller('ToBuyController',ToBuyController)
 .controller('AlreadyBoughtController',AlreadyBoughtController)
 .service('ShoppingListCheckOffService',ShoppingListCheckOffService)
- 
+
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var itemToBuy = this;
+
+  itemToBuy.items = ShoppingListCheckOffService.getItemToBuy();
+  itemToBuy.empty = ShoppingListCheckOffService.isToBuyEmpty();
+  itemToBuy.buyItem = function (itemIndex){
+     ShoppingListCheckOffService.buyItem(itemIndex);
+     itemToBuy.empty = ShoppingListCheckOffService.isToBuyEmpty();
+   };
+}
+
+AlreadyBoughtController.$ingect = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService){
+  var itemBought = this;
+
+  itemBought.items = ShoppingListCheckOffService.getItemAlreadyBought();
+}
+
+function ShoppingListCheckOffService(){
+  var service = this;
+
+  var itemToBuy = [
+    {name: "cookies", quantity: 11},
+    {name: "cookies", quantity: 12},
+    {name: "cookies", quantity: 13},
+    {name: "cookies", quantity: 14},
+    {name: "cookies", quantity: 15} ];
+
+  var itemBought = [];
+
+service.getItemToBuy = function (){
+  return itemToBuy;
+};
+
+service.isToBuyEmpty = function (){
+  return itemToBuy.length !==0;
+};
+service.getItemAlreadyBought = function(){
+  return itemBought;
+};
+
+service.buyItem = function(itemIndex){
+  itemBought.push(itemToBuy[itemIndex]);
+  itemToBuy.splice(itemIndex, 1);
+} ;
+}
+
 
 })();
